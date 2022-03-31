47166bc891f1f64f71b67ecee80bd6644d89e36c
Bash_Grey <yesufubashir@gmail.com>
2022-03-31 22:18:26 +0100

    remove auto generated index number from file

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def flip_card():
 def is_known():
     to_learn.remove(current_card)
     new_data = pandas.DataFrame(to_learn)
-    new_data.to_csv("data/ words_to_learn.csv")
+    new_data.to_csv("data/ words_to_learn.csv", index=False)
     next_card()
 
 
